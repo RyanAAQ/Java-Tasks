a23248dd0945a0c56dff6f98d3735f0cfb8abb8c
Quadri <ariyoquadri4@gmail.com>
2026-04-03 11:33:00 +0100

    Doughnut5.java file

---
+++ Doughnut5.java
@@ -0,0 +1,5 @@
+public class Doughnut5{
+  public static void main(String[] args){
+        System.out.printf("Nigeria became independent in %d and is full of great minds. I am of them, my name is %s, and i weigh %d kg and i am a %s%n", 1960, "Ryan", 45, "M");
+    }
+} 
