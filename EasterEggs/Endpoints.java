58b20919fd5cbc595173a04f8a56276c24e5041e
Quadri <ariyoquadri4@gmail.com>
2026-04-08 07:27:54 +0100

    Added new files

---
+++ EasterEggs/Endpoints.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+    public class Endpoints{
+        public static void main(String[] args){ 
+
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter the number");
+    int num = input.nextInt();
+
+    if (num > 10 && < 100);
+    System.out.println(num + "is between 10 and 100");
+
+   else 
+    System.out.println(num + "is not between 10 and 100");
+
+    }
+
+
+}
