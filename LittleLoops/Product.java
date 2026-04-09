ff91b9c60fe3c9ea6ed8c409faa988f662607939
Ryan <ariyoquadri4@gmail.com>
2026-04-09 08:15:48 +0100

    Added new files

---
+++ LittleLoops/Product.java
@@ -0,0 +1,21 @@
+public class Product{
+    public static void main(String[] args){
+
+    int n = 1;
+    int product = 1;
+
+    while (n <= 10) {
+    product = product * n;
+        
+    ++n;
+
+    System.out.println(product);
+
+        }
+
+
+    }
+
+
+
+}
