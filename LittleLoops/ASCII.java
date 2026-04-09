e3448a1c9ddced5014e05a4bbdc1cba0d85c18b3
Ryan <ariyoquadri4@gmail.com>
2026-04-09 08:09:36 +0100

    Added new files

---
+++ LittleLoops/ASCII.java
@@ -0,0 +1,10 @@
+public class ASCII{
+    public static void main(String[] args){
+
+    for (char character = 'A'; character <= 'Z'; ++character) {
+
+    System.out.println(character + " = " + (int) character);
+
+        }
+    }
+}
